ebfdaa7e995608e8dcdd161e4a3c0ac57710c593
Nikita Petrov <me@hrundik.ru>
2015-11-28 16:03:46 +0500

    Extracting contact list source from the ContactList component

--- src/app/ContactManagerApp.ts
+++ src/app/ContactManagerApp.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/angular2';
+import Contact from './Contact';
 import ContactList from './ContactList';
 
 @Component({
@@ -14,7 +15,7 @@ import ContactList from './ContactList';
 <div class="container">
 	<div class="row">
 	<div class="col-xs-12 main-container">
-    <contactList></contactList>
+    <contactList [contacts]="contacts"></contactList>
 	</div>
 	</div>
 </div>
@@ -47,5 +48,42 @@ import ContactList from './ContactList';
     `]
 })
 export default class ContactManagerApp {
-
+  contacts = [
+    {
+      id: 1,
+      name : 'Terrence S. Hatfield',
+      tel: '651-603-1723',
+      email: 'TerrenceSHatfield@rhyta.com'
+    },
+    {
+      id: 2,
+      name : 'Chris M. Manning',
+      tel: '513-307-5859',
+      email: 'ChrisMManning@dayrep.com'
+    },
+    {
+      id: 3,
+      name : 'Ricky M. Digiacomo',
+      tel: '918-774-0199',
+      email: 'RickyMDigiacomo@teleworm.us'
+    },
+    {
+      id: 4,
+      name : 'Michael K. Bayne',
+      tel: '702-989-5145',
+      email: 'MichaelKBayne@rhyta.com'
+    },
+    {
+      id: 5,
+      name : 'John I. Wilson',
+      tel: '318-292-6700',
+      email: 'JohnIWilson@dayrep.com'
+    },
+    {
+      id: 6,
+      name : 'Rodolfo P. Robinett',
+      tel: '803-557-9815',
+      email: 'RodolfoPRobinett@jourrapide.com'
+    }
+  ].map(Contact.fromObject);
 }
