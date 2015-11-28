6ca47c832bb504ebc9ee96e965f71501cd9071d5
Nikita Petrov <me@hrundik.ru>
2015-11-28 14:14:51 +0500

    Adding project stub to start with

---
+++ src/app/ContactManagerApp.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'contactManagerApp',
+    template: '<h1>Contact Manager</h1>'
+})
+export default class ContactManagerApp {
+	
+}
---
+++ src/app/app.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/angular2';
+import ContactManagerApp from './ContactManagerApp';
+
+bootstrap(ContactManagerApp);
