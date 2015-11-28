780b79065c8feaac18a909977e36e96397dc4335
Nikita Petrov <me@hrundik.ru>
2015-11-28 15:00:27 +0500

    Simple inplace contact list

--- src/app/ContactList.ts
+++ src/app/ContactList.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/angular2';
+import {Component, CORE_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
     selector: 'contactList',
@@ -7,9 +7,50 @@ import {Component} from 'angular2/angular2';
 <p class="text-center">
   <a href="#contacts/new" class="btn btn-lg btn-outline">Add Contact</a>
 </p>
-<ul class="media-list row contacts-container"></ul>
+<ul class="media-list row contacts-container">
+  <li *ng-for="#contact of contacts">
+    <span>{{contact.name}}</span>
+  </li>
+</ul>
     `
 })
 export default class ContactList {
-
+  contacts = [
+    {
+      id: 1,
+      name : 'Terrence S. Hatfield',
+      tel: '651-603-1723',
+      email: 'TerrenceSHatfield@rhyta.com'
+    },
+    {
+      id: 2,
+      name : 'Chris M. Manning',
+      tel: '513-307-5859',
+      email: 'ChrisMManning@dayrep.com'
+    },
+    {
+      id: 3,
+      name : 'Ricky M. Digiacomo',
+      tel: '918-774-0199',
+      email: 'RickyMDigiacomo@teleworm.us'
+    },
+    {
+      id: 4,
+      name : 'Michael K. Bayne',
+      tel: '702-989-5145',
+      email: 'MichaelKBayne@rhyta.com'
+    },
+    {
+      id: 5,
+      name : 'John I. Wilson',
+      tel: '318-292-6700',
+      email: 'JohnIWilson@dayrep.com'
+    },
+    {
+      id: 6,
+      name : 'Rodolfo P. Robinett',
+      tel: '803-557-9815',
+      email: 'RodolfoPRobinett@jourrapide.com'
+    }
+  ]
 }
