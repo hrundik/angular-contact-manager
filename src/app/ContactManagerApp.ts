6cd1b9e5b698537d00b28aeacbe1399108093d57
Nikita Petrov <me@hrundik.ru>
2015-11-28 14:24:34 +0500

    Adding page header and original styles

--- src/app/ContactManagerApp.ts
+++ src/app/ContactManagerApp.ts
@@ -2,7 +2,20 @@ import {Component} from 'angular2/angular2';
 
 @Component({
     selector: 'contactManagerApp',
-    template: '<h1>Contact Manager</h1>'
+    template: `
+    <header class="bs-header">
+      <div class="container">
+        <h1>Contact Manager</h1>
+        <p>Simple Angular 2 example application</p>
+      </div>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 main-container">
+        </div>
+      </div>
+    </div>
+    `
 })
 export default class ContactManagerApp {
 	
