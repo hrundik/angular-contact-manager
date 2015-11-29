7d9ffaab3d13c7340f2e78531950b855269ddc6f
Nikita Petrov <me@hrundik.ru>
2015-11-29 16:40:42 +0500

    Testing Contact class

--- src/app/contact/Contact.spec.ts
+++ src/app/contact/Contact.spec.ts
@@ -1,13 +1,30 @@
 import Contact from './Contact';
 
 describe("Contact", () => {
+  const contactSource = {
+    id: 1,
+    name: "Name",
+    tel: "1234556",
+    email: "mail@mail.com"
+  };
+  let contact:Contact;
+
+  beforeEach(() => {
+      contact = Contact.fromObject(contactSource);
+  });
+
   it('should create Contact', () => {
-    let obj = {
-      id: 1,
-      name: "Name",
-      tel: "1234556",
-      email: "mail@mail.com"
-    };
-    expect(Contact.fromObject(obj)).toEqual(jasmine.objectContaining(obj));
+    expect(contact).toEqual(jasmine.objectContaining(contactSource));
+  });
+
+  it('should clone properly', () => {
+    expect(contact.clone()).toEqual(contact);
+  });
+
+  it('should really clone', () => {
+    let clone = contact.clone();
+    clone.name = "Noname";
+    expect(clone.name).toEqual("Noname");
+    expect(contact.name).toEqual(contactSource.name);
   });
 });
